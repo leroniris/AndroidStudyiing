b1808100df2886d2f8a18dc34d95e553ba91c5d9
tom_glb <goodMorning_glb@atguigu.com>
2019-07-30 09:50:22 +0800

    Test1Android

--- app/src/main/java/ligan/com/codeview/MainActivity.java
+++ app/src/main/java/ligan/com/codeview/MainActivity.java
@@ -2,12 +2,41 @@ package ligan.com.codeview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //创建一个线性布局管理器
+        LinearLayout layout = new LinearLayout(this);
+        //设置该Activity显示layout
+        super.setContentView(layout);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        //创建一个TextView
+        final TextView show = new TextView(this);
+        //创建一个按钮
+        Button bn = new Button(this);
+        bn.setText(R.string.ok);
+        bn.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+        //向layout容器中添加TextView
+        layout.addView(show);
+        layout.addView(bn);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show.setText("Hello , Android, " + new java.util.Date());
+            }
+        });
+
     }
 }
